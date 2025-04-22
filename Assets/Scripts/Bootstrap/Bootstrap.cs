8401b7a82ef5f16f8759b82aed9eb34e5b837e19
Owen Mundy <omundy@gmail.com>
2025-04-22 13:02:24 -0400

    Update project name and bootstrap test

--- Assets/Scripts/Bootstrap/Bootstrap.cs
+++ Assets/Scripts/Bootstrap/Bootstrap.cs
@@ -3,15 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Bootstrap : MonoBehaviour
 {
-    // You could use these to make setting isHost automatic...
-    // public bool isHost = false;
-    // void Awake()
-    // {
-    // 	Globals.isHost = isHost;
-    // }
-
-    // but, using buttons instead
-
     public void SetHost()
     {
         Globals.isHost = true;
